fcbdfc716ae51390d7a0acf45a68463cd0d0ae34
Pieter E Smit <diepes@github.com>
2022-12-06 22:35:50 +1300

    add entrypointBackupRestore.sh not working

--- moinmoin/entrypoint.sh
+++ moinmoin/entrypoint.sh
@@ -1,8 +1,10 @@
-#! /bin/sh
+#! /bin/bash
 
 export GATEWAY=$(ip route list | awk '/default/ { print $3 }')
 
 if [ -z "$*" ]; then
+    ls -l /entry*
+    #source /entrypointBackupRestore.sh
     exec gunicorn -b :8080 --forwarded-allow-ips="*" moin_wsgi
 else
     exec "$@"
