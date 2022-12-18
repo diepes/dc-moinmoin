7b7c52d1bba538a3e4ffa2ab218fbd170f7c32d5
Pieter E Smit <diepes@github.com>
2022-12-18 21:34:10 +1300

    entrypointBackup.sh remove pv from pipe and add test upload

--- dbbackup/entrypointBackup.sh
+++ dbbackup/entrypointBackup.sh
@@ -2,22 +2,27 @@
 echo "# entrypointBackup.sh from $0"
 source /entrypointConfig.sh
 
-set -x 
+set -x
+echo "#Check if ls works ..."
+aws s3 ls s3://${S3_BUCKET_NAME}/
+aws s3 cp /entrypointBackup.sh s3://${S3_BUCKET_NAME}/test.txt
+echo "#start backup ..."
 # 2022-12-08 tar error:: tar: data/event-log: file changed as we read it
+#pv -t -r -b -s $(du -sb ${tardirectory} | awk '{print $1}') \
+#   --cursor --name "tar" --fineta |\
+#pv -t -r -b --wait \
+#   --cursor --name "xz" |\
 nice -n18 tar --create -f - \
               --sparse --recursion \
               -C ${tardirectory} \
               --exclude "data/event-log" \
               data |\
-pv -t -r -b -s $(du -sb ${tardirectory} | awk '{print $1}') \
-   --cursor --name "tar" --fineta |\
-nice -n19 xz --compress -6 \
+nice -n19 xz --compress -5 \
              --check=crc64 \
              --memlimit=200MiB - |\
-pv -t -r -b --wait \
-   --cursor --name "xz" |\
-aws s3 cp --expected-size $((1024*1024*300)) - s3://${S3_BUCKET_NAME}/${f}
+aws s3 cp - s3://${S3_BUCKET_NAME}/${f}
 
+# --expected-size $((1024*1024*300)) 
 # Now check if latest file is the one we just uploaded
 FILE=$( aws s3api list-objects-v2 \
           --bucket "${S3_BUCKET_NAME}" \
