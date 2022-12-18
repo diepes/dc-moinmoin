e046e2816e6c10cd5316b8eefcdaaa0b779c0caa
Pieter E Smit <diepes@github.com>
2022-12-18 20:42:18 +1300

    backup change s3 bucket to backups.vigor.nz

--- dbbackup/entrypointConfig.sh
+++ dbbackup/entrypointConfig.sh
@@ -5,10 +5,10 @@ export GATEWAY=$(ip route list | awk '/default/ { print $3 }')
 
 tardirectory="/opt/share/moin"
 
-basefilename="backupMoin${backupHostname:-Vigor}"
+basefilename="moinmoin"
 fullName="${basefilename}-$d" ##Create new baseName+date
 d="$(env TZ=Pacific/Auckland date +%F-%Hh%M%z)"
-f="${basefilename}/$(date +%Y)/${basefilename}-$d.tar.xz"
+f="${basefilename}/$(date +%Y)/$d-${basefilename}-${backupHostname:-vigor}.tar.xz"
 
 #S3_BUCKET_NAME="backupVigor"
 S3_BUCKET_NAME="backups.vigor.nz"
